f2531c457b6d3481e8e8803842d3711d6a0fb77b
Louie-Cipher <louie.spena@outlook.com>
2021-07-27 20:42:41 -0300

    dm Allow invite command

--- commands/geral/invite.js
+++ commands/geral/invite.js
@@ -4,16 +4,17 @@ module.exports = {
   name: 'invite',
   aliases: ['convite', 'link'],
   description: "link para adcionar o bot ao seu servidor",
+  dmAllow: true,
 
   async execute(client, message, args) {
 
-    var link = 'https://discord.com/api/oauth2/authorize?client_id=832275775645417553&permissions=8&scope=bot';
+    var oauthlink = 'https://discord.com/api/oauth2/authorize?client_id=832275775645417553&permissions=8&scope=bot';
 
     let embed = new Discord.MessageEmbed()
       .setColor('#00ffff')
       .setTitle('🌙 Adcione a LunaBot ao seu servidor 🌙')
-      .setDescription(`Para adcionar a LunaBot em seu servidor [clique aqui](${link})
-      \nCaso deseje, acesse também o servidor de suporte e demais interações da Luna: https://discord.gg/VFJAqE7Uz6`);
+      .setDescription(`Para adcionar a LunaBot em seu servidor [clique aqui](${oauthlink})
+      \nCaso deseje, acesse também o servidor de suporte e demais interações da Luna: [LunaLab](https://discord.gg/VFJAqE7Uz6)`);
 
     message.channel.send(embed);
 
