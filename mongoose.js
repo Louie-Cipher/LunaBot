aea5b28c7607a43746dd70007fa42aee53a1ae03
Louie-Cipher <louie.spena@outlook.com>
2021-07-27 19:06:06 -0300

    code clean

--- mongoose.js
+++ mongoose.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
-const mongodb_login = process.env.mongodb_login;
+require('dotenv').config();
 
 module.exports = {
 
   init: () => {
 
-  mongoose.connect(mongodb_login, {
+  mongoose.connect(process.env.mongodb_login, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
   })
-
-
-  /*mongoose.set('useFindAndModify', false);
-  mongoose.Promisse = global.Promisse;*/
 
   mongoose.connection.on('connected', () => {
     console.log('\x1b[4m%s\x1b[0m', '| Conectado ao 🏧 de 🎲 | ');
