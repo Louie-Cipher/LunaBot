9bb518452f48b3743af347f380a8be7323e98c5e
Louie-Cipher <louie.spena@outlook.com>
2021-07-28 15:51:09 -0300

    minor changes

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -10,19 +10,17 @@ module.exports = {
   async execute(client, message, args) {
 
     const queue = client.distube.getQueue(message)
-
-    if (!queue) return message.channel.send(
-      {embed: {color: '#ff5000', description: '❌ | Não há nenhuma música sendo reproduzida nesse momento'}}
+    if (!queue) return message.channel.send({embed: {
+      color: '#ff5000', description: '❌ | Não há nenhuma música sendo reproduzida nesse momento'}}
     );
 
     if( message.member.voice.channel.members.size > 2 && !message.member.permissions.has('MOVE_MEMBERS'))
-      return message.channel.send(
-        {embed: {color: '#ff5000', description: '❌ | Para parar a reprodução de música e desconectar o bot do canal de voz,\nvocê precisa estar sozinho com o bot na call, ou ter a permissão `mover membros`'}}
+      return message.channel.send( {embed: {
+        color: '#ff5000',
+        description: '❌ | Para parar a reprodução de música e desconectar o bot do canal de voz,\nvocê precisa estar sozinho com o bot na call, ou ter a permissão `mover membros`'}}
       );
 
     client.distube.stop(message);
-
-    message.channel.send({embed: {color: '#00ff50', description: '⏹ | Parando a reprodução de música'}});
-
+    message.channel.send({embed: {color: '#00ff50', description: '⏹ | Parando a reprodução de música\naté mais 🌙'}});
   }
 }
