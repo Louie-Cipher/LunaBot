9f483be1d6cb4883f7e52bbc17c82792aa55ea67
Louie-Cipher <louie.spena@outlook.com>
2021-09-05 20:34:00 -0300

    mongoose new features

--- mongoose.js
+++ mongoose.js
@@ -5,25 +5,24 @@ module.exports = {
 
   init: () => {
 
-  mongoose.connect(process.env.mongodb_login, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
+    mongoose.connect(process.env['mongodb_login'], {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+      //useFindAndModify: false,
+      //useCreateIndex: true
+    })
 
-  mongoose.connection.on('connected', () => {
-    console.log('\x1b[4m%s\x1b[0m', '| Conectado ao 🏧 de 🎲 | ');
-  });
+    mongoose.connection.on('connected', () => {
+      console.log('\x1b[4m%s\x1b[0m', '| Conectado ao 🏧 de 🎲 | ');
+    });
 
-  mongoose.connection.on('disconnected', () => {
-    console.log('❌ 🎲 | Desconectado do banco de dados | ❌ 🎲 ');
-  });
+    mongoose.connection.on('disconnected', () => {
+      console.log('❌ 🎲 | Desconectado do banco de dados | ❌ 🎲 ');
+    });
 
-  mongoose.connection.on('err', (err) => {
-    console.log('❌ 🎲 Erro ao conectar ao banco de dados: ' + err);
-  });
+    mongoose.connection.on('err', (err) => {
+      console.log('❌ 🎲 Erro ao conectar ao banco de dados: ' + err);
+    });
 
   }
-
 }
