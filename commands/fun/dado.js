334bf2ca81710400bde19e9e98974dc44bdd72c9
Louie-Cipher <louie.spena@outlook.com>
2021-09-07 15:41:44 -0300

    comando de dado reescrito do zero
    agora com suporte a 25 dados simultâneos (valor limitado por vontade própria. o limite real é infinitamente maior).

--- commands/fun/dado.js
+++ commands/fun/dado.js
@@ -1,54 +1,63 @@
 const Discord = require("discord.js");
 
+
 module.exports = {
   name: 'dado',
-  aliases: ['d', 'rolar', 'random', 'aleatorio', 'aleatório'],
-  description: "irei gerar um número aleatório dentro do intervalo determinado (se nenhum número for especificado, o dado será de 6 lados)",
-  dmAllow: true,
+  aliases: ['dice', 'd'],
+  description: "rola um dado com o número de lados especificado",
+
+  /**
+   * @param {Discord.Client} client 
+   * @param {Discord.Message} message 
+   * @param {String[]} args 
+   */
 
   async execute(client, message, args) {
 
-    var var2 = parseInt(args[1]);
-    var var3 = parseInt(args[2]);
-
-    if (args[0]) {var d1 = 6}
-    else{ var d1 = parseInt(args[0]) }
-
-    if (args[1]) {var d2 = 6}
-    else{ var d2 = parseInt(args[1]) }
-
-    if (args[0]) {var d1 = 6}
-    else{ var d1 = parseInt(args[0]) }
-
-    var total = (var1 + var2 + var3);
-
-    var d1 = Math.floor(Math.random() * var1);
-    var d2 = Math.floor(Math.random() * var2);
-    var d3 = Math.floor(Math.random() * var3);
-
-    var result = d1;
-    if (args[1]) {result = d1 + d2}
-    if (args[2]) {result = d1 + d2 + d3}
-
     let embed = new Discord.MessageEmbed()
-      .setColor('#4cd8b2')
-      .setTitle(`:game_die:  **${result}**  :game_die:`)
-      .setFooter(`valor total de todos os dados: ${total}`);
+      .setColor('BLURPLE')
+      .setTitle('🎲 DADOS 🎲')
 
-    if (args[1]) {
-      embed.addFields(
-        {name: 'Dado n°1:' , value: `${d1}`},
-        {name: 'Dado n°2:' , value: `${d2}`} );
-    }
-    if (args[2]) {
-      embed.addFields({name: 'Dado n°3:', value: `${d3}`})
-    }
+    let totalResult = 0;
 
-    //.addFields( {name: 'Dado um:' , value: d1 , true}, {name: 'Dado dois:' , value: d2 , true} )
+    if (!args[0]) {
+      let d1 = Math.random() * 6;
+      let result = Math.ceil(d1);
+      embed.setDescription(`resultado: ${result}`);
+    }
+    else if (args[0] && !args[1]) {
+      let d1 = Math.random() * parseInt(args[0], 10);
+      let result = Math.ceil(d1);
+      embed.setDescription(`resultado: ${result}`);
+    }
+    else if (args.length > 25) {
+      return message.reply({ content: `A quantidade máxima de dados para rolar é de 25 dados simultâneos\nO Tamanho máximo possível de todos os dados é 1×10³² ou 1e32 ou 100.000.000.000.000.000.000.000.000.000.000` });
+    }
+    else {
+
+      let i = 1;
+      for (let arg of args) {
+
+        let dado = parseInt(arg, 10);
+        if (dado == NaN || arg.length > 31) return message.reply({ content: `O valor "${arg}" do dado N° ${a} é inválido` });
+
+        let result = Math.ceil(Math.random() * dado);
+
+        totalResult += result;
+
+        if (totalResult > 1e32)
+          return message.reply({ content: `A quantidade máxima de dados para rolar é de 25 dados simultâneos\nO Tamanho máximo possível de todos os dados é 1×10³² ou 1e32 ou 100.000.000.000.000.000.000.000.000.000.000` });
+
+        embed.addField(`Dado n°${i}`, result.toString(), false);
+
+        i++
+      }
+
+      embed.setDescription(`TOTAL: ${totalResult}`)
+
+    }
     
-
-    message.channel.send(embed);
-
+    message.reply({ embeds: [embed] })
 
   }
 }
