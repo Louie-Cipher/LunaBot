aaafbeecae02ddd77e4f5b09139f95ad43c30df4
Louie-Cipher <louie.spena@outlook.com>
2021-09-08 17:09:47 -0300

    use buttons

--- commands/geral/invite.js
+++ commands/geral/invite.js
@@ -2,21 +2,33 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: 'invite',
-  aliases: ['convite', 'link'],
-  description: "link para adcionar o bot ao seu servidor",
+  aliases: ['convite', 'link', 'support'],
+  description: "link para adicionar o bot ao seu servidor",
   dmAllow: true,
 
   async execute(client, message, args) {
 
-    var oauthlink = 'https://discord.com/api/oauth2/authorize?client_id=832275775645417553&permissions=8&scope=bot';
+    const oauthLink = 'https://discord.com/api/oauth2/authorize?client_id=832275775645417553&permissions=8&scope=bot';
+    const serverInvite = 'https://discord.gg/VFJAqE7Uz6';
 
     let embed = new Discord.MessageEmbed()
       .setColor('#00ffff')
-      .setTitle('🌙 Adcione a LunaBot ao seu servidor 🌙')
-      .setDescription(`Para adcionar a LunaBot em seu servidor [clique aqui](${oauthlink})
-      \nCaso deseje, acesse também o servidor de suporte e demais interações da Luna: [LunaLab](https://discord.gg/VFJAqE7Uz6)`);
+      .setTitle('🌙 Adicione a LunaBot ao seu servidor 🌙')
+      .setDescription(`Clique nos botões abaixo para me adicionar em seu servidor, ou para entrar no meu servidor de suporte ou demais interações: LunaLab!`);
 
-    message.reply({ embeds: [embed] });
+    let buttons = new Discord.MessageActionRow()
+      .addComponents(
+        new Discord.MessageButton()
+          .setStyle('LINK')
+          .setLabel('Adicionar a Luna em seu servidor')
+          .setURL(oauthLink),
+        new Discord.MessageButton()
+          .setStyle('LINK')
+          .setLabel('Servidor de suporte e interações')
+          .setURL(serverInvite)
+      )
+
+    message.reply({ embeds: [embed], components: [buttons] });
 
   }
 }
